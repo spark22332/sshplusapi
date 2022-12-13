5e221eed5291fde0fb9eaf1cf6252f703ac4c346
Atlasssh <90170743+spark22332@users.noreply.github.com>
2022-12-13 12:04:34 -0300

    Update sshplusinst.sh

--- sshplusinst.sh
+++ sshplusinst.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+wget https://raw.githubusercontent.com/spark22332/SSHPLUS/master/remove.sh
+chmod 777 remove.sh
 mkdir -m 777 /var/www/html/pagamento
 cd /var/www/html/pagamento && wget https://cdn.discordapp.com/attachments/968040569769181194/1052230295383978075/sshplus.zip && unzip sshplus.zip
 echo -e " \e[0;31mInstalado com Sucesso \e[0m"
